381ce6e6f02aaee5113f2dda2fa09ccb7f535c4d
Shashank <48438739+shashankshet@users.noreply.github.com>
2021-04-13 14:17:48 +0530

    Add files via upload

---
+++ URLify.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class URLify
+{
+	public static void main(String [] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a string: ");	
+	String raw_string = sc.nextLine();
+	int trueLenght = raw_string.length();
+	System.out.println(getURLValue(raw_string, trueLenght));
+}
+
+
+public static String getURLValue(String str, int trueLength) {
+	StringBuilder result = new StringBuilder(); // String builder is a mutable sequence of characters, it is used when we want to modify a string in java.
+	for(int i =0; i < trueLength; i++) {
+		char value = str.charAt(i);
+		if(Character.isAlphabetic(value)) {
+			result.append(value);
+		}
+		else
+			result.append("%20");
+	}				
+	return result.toString();
+}
+}
+
